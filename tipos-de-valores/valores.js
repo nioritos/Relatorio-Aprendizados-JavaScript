08de7bd6a2c553f0f8ace024eed885a4b835253e
Nior <77200196+nioritos@users.noreply.github.com>
2022-01-04 22:13:32 -0300

    </loading files...>

---
+++ tipos-de-valores/valores.js
@@ -0,0 +1,9 @@
+var stringDupla = "ola"; // String é um tipo de valor que se apresenta como uma cadeia de caracteres entre aspas simples ('') e aspas duplas (""), como podem ver, este tipo de valor/caracteres representado está entre "".
+var stringSimples = 'ola'; // está é a string de aspas simples.
+var number = 12313123; // Number é um tipo de valor que apresenta em números, porém sem as aspas, tendo duas formas de apresenta-los, sendo o numero inteiro (que é o que está sendo apresentado), e o numero real/quebrado,, que será aprensentado agora.
+var realNumber = 123.123123; //Numeros reais/quebrados, em todas as linguagens (por conta do inglês), a virgula que teria para representar certamente o numéro real, é substituida pelo " . ";
+var valueUndefined = undefined; // O tipo de valor undefined, quer dizer que tem um tipo de valor, porém um valor *indefinido*, não se sabe qual tipo de valor é, de forma resumida.
+var valueNull = null; //O tipo de valor null, é feito para apresenta o valor de uma determinada variável, que está vazia, sem valores, sem nada.
+var valueAlternativaNull; // este tipo de declaração, é como se fosse o null, porém não precisa colocar o tipo de valor, pois já que seria um null (que não representa nada), é uma alternativa.
+var valueObjects = { }; // este tipo de valor, é um agrupamente de diversos valores entre chaves, podendo armazenar, functions(funções), arrays(listas), strings , numbers, e dentre outros valores...
+ 
---
+++ variavéis-e-tipos/variaveis.js
@@ -0,0 +1,5 @@
+var nomeVariavel; //var é um tipo de variável que pode ter seu valor trocado, porém de *escopo local*.
+let nomeVariavel; //let é um tipo de variável que pode ter seu valor trocado, porém de *escopo global*
+const nomeVariavel; //const é um tipo de variável que não pode ter seu valor trocado, porém de *escopo global*
+
+// A palavra 'escopo', se refere ao local, onde pode ser acessada, usada e se tem a sua existência. Logo logo veremos mais sobre escopo local e global.
