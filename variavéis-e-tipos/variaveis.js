b01de25956842b5edb368dd68b5a452223a4ad00
Nior <77200196+nioritos@users.noreply.github.com>
2022-01-05 00:22:02 -0300

    </update this file>
    adicionado (mais explicação e a didática) e corrigida erros sobre a variável var e let.

--- variavéis-e-tipos/variaveis.js
+++ variavéis-e-tipos/variaveis.js
@@ -1,9 +1,77 @@
-var stringDupla = "ola"; // String é um tipo de valor que se apresenta como uma cadeia de caracteres entre aspas simples ('') e aspas duplas (""), como podem ver, este tipo de valor/caracteres representado está entre "".
-var stringSimples = 'ola'; // está é a string de aspas simples.
-var number = 12313123; // Number é um tipo de valor que apresenta em números, porém sem as aspas, tendo duas formas de apresenta-los, sendo o numero inteiro (que é o que está sendo apresentado), e o numero real/quebrado,, que será aprensentado agora.
-var realNumber = 123.123123; //Numeros reais/quebrados, em todas as linguagens (por conta do inglês), a virgula que teria para representar certamente o numéro real, é substituida pelo " . ";
-var valueUndefined = undefined; // O tipo de valor undefined, quer dizer que tem um tipo de valor, porém um valor *indefinido*, não se sabe qual tipo de valor é, de forma resumida.
-var valueNull = null; //O tipo de valor null, é feito para apresenta o valor de uma determinada variável, que está vazia, sem valores, sem nada.
-var valueAlternativaNull; // este tipo de declaração, é como se fosse o null, porém não precisa colocar o tipo de valor, pois já que seria um null (que não representa nada), é uma alternativa.
-var valueObjects = { }; // este tipo de valor, é um agrupamente de diversos valores entre chaves, podendo armazenar, functions(funções), arrays(listas), strings , numbers, e dentre outros valores...
-var symbol = Symbol(); //...
+var nomeVariavel; //var é um tipo de variável que pode ter seu valor trocado, porém com "destinos/modos de usar o escopo" e "funcções internas" e uma certa liberdade. Como o hosting, podendo ter 2 variáveis com o mesmo nome "sem problemas (sem retornar que a variaável com o nome já existe)"=.
+let nameVariavel; //let é um tipo de variável que pode ter seu valor trocado, porém com restrições que o var tem a liberdade de usar. Mas especificamente, o let é posto em um bloco de código, ele irá trabalhar apenas neste bloco .
+// exemplo : 
+
+//hoisting do var
+
+console.log(aMenssage)
+var aMenssage = 'oi';
+
+
+// o que acontece : 
+
+// var aMenssage;
+// console.log(aMenssage); 
+//o que acontece é a elevação da váriavel sem o seu valor para antes do console.log(), que assim, não retorna um ReferenceError (erro de referência).
+function retEscopo() {
+    for(var i = 4;i < 5; i++) {
+        console.log(i)
+    }
+
+console.log('////////////////////////////////////////////////////////////////////////////////////////// separation')
+
+    if(i === 3) {
+        return(
+            console.log(true)
+        )
+    }else {
+        return (
+            console.log(false)
+        )
+    }
+
+    //retorna um dos argumentos, pois o var e seu valor, estão disponíveis.
+}
+
+retEscopo();
+
+console.log('////////////////////////////////////////////////////////////////////////////////////////// separation')
+
+
+function retLetEscopo() {
+    for(let i = 0; i < 5; i++) {
+        console.log(i)
+    }
+
+console.log('////////////////////////////////////////////////////////////////////////////////////////// separation')
+
+
+    /*     if(i === 0) {
+        return(
+            console.log(true)
+        )
+    }else{
+        return(
+            console.log(false)
+        )
+    }       */
+
+    // retorna undenifed, pois o let e seu valor não estão disponíveis
+
+};
+
+retLetEscopo();
+
+
+/*A diferença é que  (recomendo rodar o código):
+    O var, fora do bloco de código (neste caso, é o for()), eu posso acessa-la e consequentemente, se eu quiser, eu posso trata-la.
+    Porém, com o let, fora do bloco de código, eu NÃO posso acessa-la, pois o let quando posto em um bloco de código, ele apenas fica acessível, dentro do bloco. Diferente do var, que fica sim disponivél, já que trabalha apenas com escopo local e global.
+    */
+
+
+const nomeVariavelConstante = 0; // const é um tipo de variável que não pode ter seu valor trocado.
+
+/*nomeVariavelConstante = 1; */
+console.log(nomeVariavelConstante);
+
+//retorna ERROR de substituição de valor de uma variável constante, pois não pode ser trocada.
